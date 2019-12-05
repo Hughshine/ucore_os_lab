62d32858eee68330f4189c1f54664e881d6b83f0
shrine <1015545250@qq.com>
2019-12-05 15:39:09 +0800

    feat(lab7): not tested

--- labcodes/lab7/kern/sync/check_sync.c
+++ labcodes/lab7/kern/sync/check_sync.c
@@ -184,6 +184,11 @@ void phi_take_forks_condvar(int i) {
      // LAB7 EXERCISE1: YOUR CODE
      // I am hungry
      // try to get fork
+     phi_test_condvar(i);
+     if(state_condvar[i]!=EATING)
+     {
+         cond_wait(&(mtp->cv[i]));
+     }
 //--------leave routine in monitor--------------
       if(mtp->next_count>0)
          up(&(mtp->next));
@@ -193,11 +198,14 @@ void phi_take_forks_condvar(int i) {
 
 void phi_put_forks_condvar(int i) {
      down(&(mtp->mutex));
-
 //--------into routine in monitor--------------
      // LAB7 EXERCISE1: YOUR CODE
      // I ate over
      // test left and right neighbors
+    down(&(mtp->mutex));
+    state_condvar[i] = THINKING;
+    phi_test_condvar(LEFT);
+    phi_test_condvar(RIGHT);
 //--------leave routine in monitor--------------
      if(mtp->next_count>0)
         up(&(mtp->next));
